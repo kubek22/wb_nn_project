d8e9c933818739d2d46e8394a58a54265411bd00
katebokhan <@kateqwerty001>
2024-05-28 01:37:54 +0200

    the best models, the last versions, the best learning rates were chosen

--- CNNmodels/Resnet18/task/model.py
+++ CNNmodels/Resnet18/task/model.py
@@ -1,10 +1,12 @@
+#final
+
 from torch import nn
 import torchvision.models as models
 
 class ResNet18(nn.Module):
     def __init__(self, num_classes=47):
         super(ResNet18, self).__init__()
-        self.resnet18 = models.resnet18(pretrained=False)
+        self.resnet18 = models.resnet18(weights = None)
         self.resnet18.fc = nn.Linear(self.resnet18.fc.in_features, num_classes)
 
     def forward(self, x):
